713284739cad822e522071e6a519b2e0db463189
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-02 10:22:39 -0500

    adds remaining algo for kata

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -5,7 +5,19 @@ public class RomanNumeral {
   public String converter(int num) {
     String roman = "";
     while (num > 0) {
-      if (num >= 10) {
+      if (num >= 1000) {
+        roman += "M";
+        num -= 1000;
+      } else if (num >= 500) {
+        roman += "D";
+        num -= 500;
+      } else if (num >= 100) {
+        roman += "C";
+        num -= 100;
+      } else if (num >= 50) {
+        roman += "L";
+        num -= 50;
+      } else if (num >= 10) {
         roman += "X";
         num = num - 10;
       } else if (num >= 5) {
