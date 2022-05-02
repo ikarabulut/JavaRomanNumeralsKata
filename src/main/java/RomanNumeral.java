1ba2d409ecf1c19009912e85da7d0ccbeaa36361
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-02 10:10:14 -0500

    adds to algo to accept X for 10

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -5,16 +5,19 @@ public class RomanNumeral {
   public String converter(int num) {
     String roman = "";
     while (num > 0) {
-      if (num >= 5) {
+      if (num >= 10) {
+        roman += "X";
+        num = num - 10;
+      } else if (num >= 5) {
         roman += "V";
         num = num - 5;
       } else if (num > 0) {
         roman += "I";
         num = num - 1;
       }
-
     }
   return roman;
   }
+
 }
 
