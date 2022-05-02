602da12b021e53f4929785abc4b675bc094f76de
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-02 08:49:58 -0500

    updates algo to get test passing

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,16 +1,20 @@
 package com.ikarabulut;
 
 public class RomanNumeral {
-  public String single = "I";
-  public String five = "V";
 
   public String converter(int num) {
     String roman = "";
-    for (int i = 0; i < num; i++) {
-      roman += single;
-    }
-    return roman;
-  }
+    while (num > 0) {
+      if (num >= 5) {
+        roman += "V";
+        num = num - 5;
+      } else if (num > 0) {
+        roman += "I";
+        num = num - 1;
+      }
 
+    }
+  return roman;
+  }
 }
 
